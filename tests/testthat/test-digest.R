7c8f685c325e5c5b17cf4f3acbcf7bb8275aabde
Remy Dumas <remy.dumas84@gmail.com>
2026-07-31 23:30:10 +0200

    completed digestion tests for numerical data

--- tests/testthat/test-digest.R
+++ tests/testthat/test-digest.R
@@ -1,3 +1,5 @@
+vars = c('gender', 'happy', 'occupation', 'symptoms', 'age')
+
 test_that("params reduction works", {
   params = rbind(myparams$stats, myparams$stats) # Introduce double entries
   expect_false(check_unicity(params %>% mutate(i=row_number()), var = 'i',  id_cols = c('var_type', 'stat_name')))
@@ -7,10 +9,7 @@ test_that("params reduction works", {
 
 
 test_that("check digestion", {
-  vars = c('age', 'gender', 'happy', 'occupation', 'symptoms')
-  # vars = c('gender', 'happy', 'symptoms')
-
-  gourmex = mount(vars, 'id', params=myparams) %>%
+  gourmex = mount(vars, 'id', params=myparams0) %>%
     ingest(df_test) %>%
     digest()
 
@@ -32,9 +31,7 @@ test_that("check digestion", {
 
 
 test_that("check digestion with groups", {
-  vars = c('age', 'gender', 'happy', 'occupation', 'symptoms')
-
-  gourmex = mount(vars, 'id', group_cols = 'group', params=myparams2) %>%
+  gourmex = mount(vars, 'id', group_cols = 'group', params=myparams) %>%
     ingest(df_test) %>%
     digest()
 
@@ -54,4 +51,31 @@ test_that("check digestion with groups", {
     expect_all_true(stat_names %in% names(gourmex$storage$stats[[var]]))
   }
 })
+
+
+test_that("check digestion with groups", {
+  vars = c('age', 'gender', 'happy', 'occupation', 'symptoms')
+
+  gourmex = mount(vars, 'id', group_cols = 'group', params=myparams) %>%
+    ingest(df_test) %>%
+    digest()
+
+  expect_setequal(names(gourmex$storage$stats), gourmex$params$vars)
+
+  num_stats = c('mean', 'std', 'SE', 'CI95', 'min', 'q1', 'median', 'q3', 'max')
+  num_stats_group = c('mean', 'std', 'SE', 'CI95', 'min', 'q1', 'median', 'q3', 'max') %>% paste0('_group')
+
+  for(var in gourmex$types$var){
+    var_type = gourmex$types %>% filter(var == .env$var) %>% pull(var_type)
+    stat_names = gourmex$params$stats %>% filter(var_type == .env$var_type) %>% pull(stat_name)
+    if('num_stats' %in% stat_names)
+      stat_names = stat_names %>% setdiff('num_stats') %>% c(num_stats)
+    if('num_stats_group' %in% stat_names)
+      stat_names = stat_names %>% setdiff('num_stats_group') %>% c(num_stats_group)
+
+    expect_all_true(stat_names %in% names(gourmex$storage$stats[[var]]))
+  }
+})
+
+
 
