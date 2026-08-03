f356c9d1ce5c9e99e9be7ab808cfb6b888e50a85
Remy Dumas <remy.dumas84@gmail.com>
2026-08-03 12:50:14 +0200

    removed group_expr and strata_expr

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -42,38 +42,36 @@ df_test = tibble(
 
 # ---
 # TO REMOVE
-group_expr = expr(gourmex$params$group_cols)
-strata_expr = expr(gourmex$params$strata_cols)
 
 stat_funcs_cat = list(
-  N = expr(\(...) stat_total(..., by=eval(strata_expr))),
-  n_avail = expr(\(...) stat_n_avail(..., by=eval(strata_expr))),
-  n_level = expr(\(...) stat_total(..., by= c(var, eval(strata_expr)))),
-  n_missing = expr(\(...) stat_n_miss(..., by=eval(strata_expr))),
-  p_level = expr(\(...) stat_perc(..., numerator_col = 'n_level', denominator_col = 'n_avail', by=eval(strata_expr))))
+  N = expr(\(...) stat_total(..., by=gourmex$params$strata_cols)),
+  n_avail = expr(\(...) stat_n_avail(..., by=gourmex$params$strata_cols)),
+  n_level = expr(\(...) stat_total(..., by= c(var, gourmex$params$strata_cols))),
+  n_missing = expr(\(...) stat_n_miss(..., by=gourmex$params$strata_cols)),
+  p_level = expr(\(...) stat_perc(..., numerator_col = 'n_level', denominator_col = 'n_avail', by=gourmex$params$strata_cols)))
 
 # for comparative analysis
 stat_funcs_cat_gp = list(
-  N_group = expr(\(...) stat_total(..., by=c(eval(group_expr), eval(strata_expr)))),
-  n_avail_group = expr(\(...) stat_n_avail(..., by=c(eval(group_expr), eval(strata_expr)))),
-  n_level_group = expr(\(...) stat_total(..., by=c(var, eval(group_expr), eval(strata_expr)))),
-  p_level_group = expr(\(...) stat_perc(..., numerator_col = 'n_level_group', denominator_col = 'n_avail_group', by=eval(strata_expr))),
-  p_value = expr(\(...) stat_pvalue(..., by=eval(group_expr), strata=eval(strata_expr)))
+  N_group = expr(\(...) stat_total(..., by=c(gourmex$params$group_cols, gourmex$params$strata_cols))),
+  n_avail_group = expr(\(...) stat_n_avail(..., by=c(gourmex$params$group_cols, gourmex$params$strata_cols))),
+  n_level_group = expr(\(...) stat_total(..., by=c(var, gourmex$params$group_cols, gourmex$params$strata_cols))),
+  p_level_group = expr(\(...) stat_perc(..., numerator_col = 'n_level_group', denominator_col = 'n_avail_group', by=gourmex$params$strata_cols)),
+  p_value = expr(\(...) stat_pvalue(..., by=gourmex$params$group_cols, strata=gourmex$params$strata_cols))
   )
 
 stat_funcs_logic = stat_funcs_cat
 stat_funcs_logic_gp = stat_funcs_cat_gp
 
 stat_funcs_num = list(
-  num_stats = expr(\(...) stat_numeric(..., by=eval(strata_expr))),
-  N = expr(\(...) stat_total(..., by=eval(strata_expr))),
-  n_avail = expr(\(...) stat_n_avail(..., by=eval(strata_expr))),
-  n_missing = expr(\(...) stat_n_miss(..., by=eval(strata_expr))))
+  num_stats = expr(\(...) stat_numeric(..., by=gourmex$params$strata_cols)),
+  N = expr(\(...) stat_total(..., by=gourmex$params$strata_cols)),
+  n_avail = expr(\(...) stat_n_avail(..., by=gourmex$params$strata_cols)),
+  n_missing = expr(\(...) stat_n_miss(..., by=gourmex$params$strata_cols)))
 stat_funcs_num_gp = list(
-  num_stats_group = expr(\(...) stat_numeric(..., by=c(eval(group_expr), eval(strata_expr)), suffix = '_group')),
-  N_group = expr(\(...) stat_total(..., by=c(eval(group_expr), eval(strata_expr)))),
-  n_avail_group = expr(\(...) stat_n_avail(..., by=c(eval(group_expr), eval(strata_expr)))),
-  p_value = expr(\(...) stat_pvalue(..., by=eval(group_expr), strata=eval(strata_expr)))
+  num_stats_group = expr(\(...) stat_numeric(..., by=c(gourmex$params$group_cols, gourmex$params$strata_cols), suffix = '_group')),
+  N_group = expr(\(...) stat_total(..., by=c(gourmex$params$group_cols, gourmex$params$strata_cols))),
+  n_avail_group = expr(\(...) stat_n_avail(..., by=c(gourmex$params$group_cols, gourmex$params$strata_cols))),
+  p_value = expr(\(...) stat_pvalue(..., by=gourmex$params$group_cols, strata=gourmex$params$strata_cols))
 )
 
 A = tibble(var_type = 'categorical', stat_name = names(stat_funcs_cat), stat_func = unname(stat_funcs_cat))
